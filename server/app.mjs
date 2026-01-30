9f0ea24118ce435d49bfabd3adf6ff14f57e7930
foyfox58 <foy9323@gmail.com>
2026-01-30 15:13:45 +0700

    Add assignment creation endpoint and update server configuration

--- server/app.mjs
+++ server/app.mjs
@@ -1,12 +1,61 @@
 import express from "express";
+import connectionPool from "./utils/db.mjs";
 
 const app = express();
 const port = 4001;
 
+// middleware
+app.use(express.json());
+
+// test route
 app.get("/test", (req, res) => {
-  return res.json("Server API is working 🚀");
+  res.json({ message: "Server API is working 🚀" });
 });
 
+// Create Assignment
+app.post("/assignments", async (req, res) => {
+  try {
+    const { title, content, category } = req.body;
+
+    // ✅ ตรวจสอบข้อมูลจาก client
+    if (!title || !content || !category) {
+      return res.status(400).json({
+        message:
+          "Server could not create assignment because there are missing data from client",
+      });
+    }
+
+    const newAssignments = {
+      title,
+      content,
+      category,
+      created_at: new Date(),
+      updated_at: new Date(),
+      published_at: new Date(),
+    };
+
+    await connectionPool.query(
+      `INSERT INTO assignments (title, content, category)
+       VALUES ($1, $2, $3)`,
+      [title, content, category]
+    );
+
+    return res.status(201).json({
+      message: "Created assignment successfully",
+    });
+
+  } catch (error) {
+    console.log(error);
+
+    // ❌ ปัญหา database / server
+    return res.status(500).json({
+      message:
+        "Server could not create assignment because database connection",
+    });
+  }
+});
+
+// start server
 app.listen(port, () => {
-  console.log(`Server is running at ${port}`);
+  console.log(`Server is running at http://localhost:${port}`);
 });
--- server/utils/db.mjs
+++ server/utils/db.mjs
@@ -1,10 +1,11 @@
 // Create PostgreSQL Connection Pool here !
-import * as pg from "pg";
+import * as pg from "pg"
 const { Pool } = pg.default;
 
 const connectionPool = new Pool({
   connectionString:
-    "postgresql://your-db-username:your-db-password@localhost:5432/your-db-name",
+    "postgresql://postgres:YourPassword@localhost:5432/test",
+    
 });
 
 export default connectionPool;
